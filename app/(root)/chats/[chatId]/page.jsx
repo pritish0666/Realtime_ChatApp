18f2b38aee6e0755a155dcc83eaf8eb8074f17ce
Pritish Patra <pritishpatra06@gmail.com>
2024-03-10 17:50:42 +0530

    minor fix

--- app/(root)/chats/[chatId]/page.jsx
+++ app/(root)/chats/[chatId]/page.jsx
@@ -1,20 +1,21 @@
-"use client";
+"use client"
 
-import { useSession } from "next-auth/react";
-import { useParams } from "next/navigation";
-import ChatList from "@components/ChatList";
-import ChatDetails from "@components/ChatDetails";
-import { useEffect } from "react";
+import ChatDetails from "@components/ChatDetails"
+import ChatList from "@components/ChatList"
+import { useSession } from "next-auth/react"
+import { useParams } from "next/navigation"
+import { useEffect } from "react"
+
 
 const chatPage = () => {
-  const { chatId } = useParams();
-  //console.log(chatId)
-  const { data: session } = useSession();
-  const currentUser = session?.user;
+  const { chatId } = useParams()
+
+  const { data: session } = useSession()
+  const currentUser = session?.user
 
   const seenMessages = async () => {
     try {
-      await fetch(`/api/chats/${chatId}`, {
+      await fetch (`/api/chats/${chatId}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -22,29 +23,22 @@ const chatPage = () => {
         body: JSON.stringify({
           currentUserId: currentUser._id
         })
-      });
-    } catch (error) {
-      console.log(error);
+      })
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    useEffect(()=>{
-      if(currentUser && chatId){
-        seenMessages()
-
-      }
-    },[currentUser,chatId])
-  };
+  useEffect(() => {
+    if (currentUser && chatId) seenMessages()
+  }, [currentUser, chatId])
 
   return (
     <div className="main-container">
-      <div className="w-1/3 max-lg:hidden">
-        <ChatList currentChatId={chatId} />
-      </div>
-      <div className="w-2/3 max-lg:w-full">
-        <ChatDetails chatId={chatId} />
-      </div>
+      <div className="w-1/3 max-lg:hidden"><ChatList currentChatId={chatId}/></div>
+      <div className="w-2/3 max-lg:w-full"><ChatDetails chatId={chatId}/></div>
     </div>
-  );
-};
+  )
+}
 
-export default chatPage;
+export default chatPage
